592d447a167b5a19de70c9a8f0c7b527dcbf0cfb
GlutenFreeGrass <kawu11@s.sfusd.edu>
2025-01-22 11:01:02 -0800

    Functioning Gun

---
+++ M10.twin_stick_workbook/weapon_pivot.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@onready var weapon_pivot: Node2D = $"."
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass 
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var aim_direction := Vector2.ZERO
+	aim_direction = global_position.direction_to(get_global_mouse_position())
+	if aim_direction.length() > 0.1:
+		rotation = aim_direction.angle()
+	
